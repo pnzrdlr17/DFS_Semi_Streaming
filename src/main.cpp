a9dfc89e9216f98476d653f62ca5499f9ae7f4c7
Fayez Anwar <fayez532552@gmail.com>
2025-01-08 18:23:21 +0530

    Checkpoint Commit - Refactor main.cpp to enhance argument parsing and add support for experiment modes including shared usage of global variables

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,33 @@
 #include <fstream>
 #include <sstream>
 #include <cstring>
+#include <vector>
 #include "./algo_runner.h"
 #include "./graph_generator.h"
+#include "./parser.h"
+#include "./experiment.h"
 #define ll long long int
+#define endl '\n'
 using namespace std;
 
-enum Mode {
-    RUN_ALGO,  // Running the algorithm
-    GEN_GRAPH  // Generating a graph
-};
+/*
+Modes:
+RUN_ALGO: Run the given algorithm for the given (filepath) graph
+GEN_GRAPH: Generate a random graph with the given parameters
+PREP_EXP: [Prerun for a particular type of experiment] -- generate all the required graphs in advance
+RUN_EXP: Run an experiment [VARN, VARM, VARK] with the given parameters
+*/
 
 Mode mode = RUN_ALGO;
 ll n, m, k = 1, seed_token = 42;
-int algorithm;
+int algorithm, experiment_type, sparsity, iterations;
 char variant = '0';
 string filePath, graph_type = "POWLAW";
 ifstream fileStream;
+string modesText = "Modes: RUN_ALGO, GEN_GRAPH, RUN_EXP, PREP_EXP\n";
+const string randomGraphsDirectory = "./input/random_graphs/";
 
- /*
+/*
 Algorithm codes:
 0 - Simple DFS {only two variants 0 and 1}  {doesn't need k}
 1 - Improved DFS {doesn't have a variant}   {doesn't need k}
@@ -27,78 +36,55 @@ Algorithm codes:
 3 - kLev DFS {needs k}
 */
 
-void parseArgs(int argc, char *argv[]) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <mode> <args>\nModes: RUN_ALGO, GEN_GRAPH\n";
-        exit(1);
-    }
+/*
+Experiment types:
+0 - VARN
+1 - VARM
+2 - VARK
+*/
 
-    string modeStr = argv[1];
-    if (modeStr == "RUN_ALGO") {
-        mode = RUN_ALGO;
-    } else if (modeStr == "GEN_GRAPH") {
-        mode = GEN_GRAPH;
-    } else {
-        cerr << "Invalid mode: " << modeStr << "\nModes: RUN_ALGO, GEN_GRAPH\n";
-        exit(1);
-    }
-
-
-    if (mode == RUN_ALGO) {
-        if (argc < 6) {
-            cerr << "Usage: " << argv[0] << "<mode:RUN_ALGO> <n> <m> <filepath> <algorithm> <variant> <k>\n";
-            exit(1);
-        }
-
-        n = stoll(argv[2]);
-        m = stoll(argv[3]);
-        filePath = argv[4];
-        algorithm = stoi(argv[5]);
-
-        if (algorithm == 0 || algorithm == 2 || algorithm == 3) {
-            if (argc < 7) {
-                cerr << "Usage: <mode:RUN_ALGO> <n> <m> <filepath> <algorithm> <variant> <k>\nVariant must be specified for simp(0), kPath(2) and kLev(3) algorithms\n";
-                exit(1);
-            }
-            variant = argv[6][0];
-        }
-
-        if (algorithm == 2 || algorithm == 3) {
-            if (argc < 8) {
-                cerr << "Usage: <mode:RUN_ALGO> <n> <m> <filepath> <algorithm> <variant> <k>\nk must be specified for kPath(2) and kLev(3) algorithms\n";
-                exit(1);
-            }
-            k = stoll(argv[7]);
-        }
-    }
-    else if (mode == GEN_GRAPH) {
-        if (argc < 4) {
-            cerr << "Usage: " << argv[0] << "<mode:GEN_GRAPH> <n> <m> <seed> <graph_generation_type>\n";
-            exit(1);
-        }
-
-        n = stoll(argv[2]);
-        m = stoll(argv[3]);
-
-        if (argc > 4) seed_token = stoll(argv[4]);
-        if (argc > 5) graph_type = argv[5];
-    }
-}
+/*
+Sparsity: M based on N
+0 - 2*N
+1 - 5*N
+2 - N*log2(N)
+3 - N*sqrt(N)
+4 - N*(N-1)/2
+*/
 
 int main(int argc, char *argv[]) {
 
-    parseArgs(argc, argv);
+    mode = parseArgs(argc, argv);
 
-    if (mode == RUN_ALGO) {
-        AlgorithmResult result = runAlgorithm(n, m, filePath, algorithm, variant, k);
-        //TODO fix n, m, k to ll in all algorithm files
+    switch (mode) {
+        case RUN_ALGO: {
+            AlgorithmResult result = runAlgorithm(n, m, filePath, algorithm, variant, k);
+            // TODO fix n, m, k to ll in all algorithm files
+            cout << "PassCount: " << result.passCount << "\n";
+            break;
+        }
+        case GEN_GRAPH: {
+            filePath = generateRandomGraph(n, m, seed_token, graph_type);
+            cout << "Generated graph at: " << filePath << "\n";
+            break;
+        }
+        case PREP_EXP: {
+            prepareExperiment(experiment_type, n, sparsity, graph_type, iterations, seed_token);
+            cout << "Seed: " << seed_token << "\n";
+            break;
+        }
+        case RUN_EXP: { //TODO: Add a check that file exists otherwise ask the user to call prep_exp
+            vector<pair<ll,double>> avgPasses = runExperiment(experiment_type, n, sparsity, graph_type, iterations, seed_token, algorithm, variant, k);
 
-        cout << "PassCount: " << result.passCount << "\n";
-    }
-    else if (mode == GEN_GRAPH) {
-        filePath = generateRandomGraph(n, m, seed_token, graph_type);
-
-        cout << "Generated graph at: " << filePath << "\n";
+            cout << "Experiment Type: " << experiment_type << endl << " Average Passes: \n";
+            for (int i = 0; i < avgPasses.size(); ++i) {
+                cout << avgPasses[i].first << " " << avgPasses[i].second << "\n";
+            }
+            break;
+        }
+        default:
+            cerr << "Invalid mode: " << mode << endl << modesText << endl;
+            exit(1);
     }
 
     return 0;
