ae21ab30e3f1b3282fd9624412d7db57ee0a2c3c
Fayez Anwar <fayez532552@gmail.com>
2025-01-24 23:56:34 +0530

    Add Friendster parser implementation and update Orkut parser instructions

--- utils/friendster_parser.cpp
+++ utils/friendster_parser.cpp
@@ -1,25 +1,14 @@
 /*
     **Instructions to run the program**
  * 1. Download the file as mentioned in graph_sources.md and extract it.
- * 2. Use the `com-orkut.ungraph.txt` file directly as input to this program.
+ * 2. Use the `com-friendster.ungraph.txt` file directly as input to this program.
  * 3. Compile the program using the following command:
- *      g++ -o orkut_parser orkut_parser.cpp
+ *      g++ -o friendster_parser friendster_parser.cpp
  * 4. Run the program with the path to the CSV file as an argument:
- *      ./orkut_parser <path_to_com-orkut.ungraph.txt>
+ *      ./friendster_parser <path_to_com-friendster.ungraph.txt>
  * 5. OR compile and run at once using:
- *      g++ -o orkut_parser orkut_parser.cpp && ./orkut_parser <path_to_com-orkut.ungraph.txt>
- * 6. You should get an output file with the name `output_orkut_graph.edg` in accordance with the required format.
-*/
-
-/*
-    **Graph Verification** -- as of 23-01-2025
-    * 1. Contains N=3072441 nodes
-    * 2. Contains M=117185083 edges
-    * 3. No self-loops found
-    * 4. No duplicate edges found
-    ! 5. Nodes labelled from 1 to 3072626 (instead of 3072441)
-        ? This is fixed by the below code and the output file has the nodes labelled from 1 to 3072441.
-    * 6. The verification logic is commented out currently and it only generates the output file.
+ *      g++ -o friendster_parser friendster_parser.cpp && ./friendster_parser <path_to_com-friendster.ungraph.txt>
+ * 6. You should get an output file with the name `output_friendster_graph.edg` in accordance with the required format.
 */
 
 #include <iostream>
@@ -27,14 +16,14 @@
 #include <fstream>
 #include <cstring>
 #include <map>
-// #include <set>
+#include <set>
 using namespace std;
 #define ll long long
 #define endl '\n'
 #define pll pair<ll,ll>
 
-const ll N = 3072441; // Number of nodes
-const ll M = 117185083; // Number of edges
+const ll N = 65608366; // Number of nodes
+const ll M = 1806067135; // Number of edges
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(0);
@@ -51,7 +40,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    string outputFileName = "output_orkut_graph.edg";
+    string outputFileName = "output_friendster_graph.edg";
     ofstream outfile(outputFileName);
 
 
@@ -59,11 +48,9 @@ int main(int argc, char *argv[]) {
     ll u, v, e = 0;
     vector<pll> edges;
     map<ll, ll> nodes;
-    /*
     ll line = 1;
     pll p;
     set<pll> duplicate_edges;
-    */
 
     for (int i = 0; i < 4; ++i) getline(infile, s); // Skip the first 4 lines of the file as they have meta data
 
@@ -74,7 +61,7 @@ int main(int argc, char *argv[]) {
 
         edges.push_back({u, v});
 
-        /*
+
         if (u == v) {
             cout << "Self-loop found at line " << line << endl;
         }
@@ -85,12 +72,12 @@ int main(int argc, char *argv[]) {
         }
         else duplicate_edges.insert(p);
         line++;
-        */
+
     }
 
     infile.close();
 
-    /*
+
     if (nodes.size() != N) {
         cout << "Number of nodes mismatched: " << nodes.size() << " instead of " << N << endl;
     }
@@ -102,7 +89,7 @@ int main(int argc, char *argv[]) {
     ll minLabel = nodes.begin()->first, maxLabel = nodes.rbegin()->first;
 
     if (minLabel != 1 || maxLabel != N) {
-        cout << "Nodes not labelled from 1 to " << N << endl;
+        cout << "Nodes not labelled from "<< minLabel << " to " << maxLabel << " instead of 1 to " << N << endl;
         if (maxLabel - minLabel + 1 == N) {
             cout << "Nodes labelled continuously, adding "<< 1ll - minLabel << " would fix it" << endl;
         }
@@ -110,9 +97,10 @@ int main(int argc, char *argv[]) {
             cout << "Nodes not labelled continuously, needs to be re-mapped" << endl;
         }
     }
+    else cout << "Nodes labelled from 1 to " << N << endl;
 
     cout << "Verification completed" << endl;
-    */
+
 
     e = 1;
     for (auto& node : nodes) node.second = e++; // Renumbering the nodes consecutively from 1 to N
